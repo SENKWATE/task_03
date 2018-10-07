f31a8b13ca2c7b05051c1438661fb4a1f6b749f8
SENKWATE <m_alsaiegh@hotmail.com>
2018-10-07 16:30:48 +0300

    task3 submission

--- restaurants/views.py
+++ restaurants/views.py
@@ -4,16 +4,22 @@ def welcome(request):
     return render(request, 'index.html', {'msg':'Hello World!'})
 
 def restaurant_list(request):
+    restaurants = [
+        {"name":"burgerKing","food":"Checken Royal"},
+        {"name":"PizzaHut","food":"Pepperone"},
+        {"name":"Macdonalds","food":"Potato"},
+        ]
 
     context = {
+        "my_list": restaurants,
+            }
 
-    }
     return render(request, 'list.html', context)
 
 
 def restaurant_detail(request):
-
+    
     context = {
-
+    'my_object': {"name":"burgerKing","food":"Checken Royal"},
     }
     return render(request, 'detail.html', context)
